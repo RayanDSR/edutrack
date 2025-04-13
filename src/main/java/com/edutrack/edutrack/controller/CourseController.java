8c76ce91d1bbdcb88be640e6723e7c9b6f0b52f2
RayanDSR <dahmanesalemrayan@gmail.com>
2025-04-13 18:56:56 +0100

    Added POST /courses to create new courses

--- src/main/java/com/edutrack/edutrack/controller/CourseController.java
+++ src/main/java/com/edutrack/edutrack/controller/CourseController.java
@@ -5,18 +5,33 @@ import com.edutrack.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class CourseController {
+    private List<Course> courses = new ArrayList<>();
+    private Long courseIdCounter = 1L;
+
+    public CourseController() {
+        courses.add(new Course(courseIdCounter++, "Intro to Java", "Learn the basics of Java programming"));
+        courses.add(new Course(courseIdCounter++, "Spring Boot 101", "Get started with Spring Boot"));
+        courses.add(new Course(courseIdCounter++, "MySQL for Beginners", "Learn how to use MySQL with Java"));
+    }
+
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return Arrays.asList(
-            new Course(1L, "Intro to Java", "Learn the basics of Java programming"),
-            new Course(2L, "Spring Boot 101", "Get started with Spring Boot"),
-            new Course(3L, "MySQL for Beginners", "Learn how to use MySQL with Java"),
-            new Course(4L, "Git Fundamentals", "Learn Git from the ground up")
-        );
+        return courses;
     }
+
+    @PostMapping("/courses")
+    public Course createCourse(@RequestBody Course newCourse) {
+        newCourse.setId(courseIdCounter++);
+        courses.add(newCourse);
+        return newCourse;
+    }
+    
 }
